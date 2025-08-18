b714ca66e3177219bd7d1eb1e302115218573435
nikitaradionov <n.frankov76@gmail.com>
2025-08-18 14:11:03 +0300

    Fix prompt and add since and until to commit requests

--- server/src/main/kotlin/ru/radionov/mindweaverserver/Application.kt
+++ server/src/main/kotlin/ru/radionov/mindweaverserver/Application.kt
@@ -7,6 +7,8 @@ import ru.radionov.mindweaverserver.aiClients.ChatGPTApiClient
 import ru.radionov.mindweaverserver.mcpClient.MCPClient
 import ru.radionov.mindweaverserver.models.ChatMessage
 import ru.radionov.mindweaverserver.utils.sendToTelegram
+import kotlin.time.Clock
+import kotlin.time.Duration
 import kotlin.time.ExperimentalTime
 
 @OptIn(ExperimentalTime::class)
@@ -20,12 +22,21 @@ fun main(): Unit = runBlocking {
     launch {
         val owner = config.githubOwnerName
         val repo = config.githubRepoName
-        val until = null
+        val since = Clock.System.now().minus(Duration.parse("24h"))
+        val until = Clock.System.now()
 
-        val report = client.summarizeCommits(owner = owner, repo = repo, until = until)
+        val report = client.summarizeCommits(owner = owner, repo = repo, since = since, until = until)
         val text = report?.mapNotNull { it.text }.orEmpty().joinToString { "\n\n" + it }
 
-        val content = "Проведи анализ коммитов и выдай статистику \n$text"
+        val content = """
+            Проведи анализ коммитов и выдай статистику.
+            В статистике не должно быть форматирования markdown. 
+          
+            НЕ ПРИДУМЫВАЙ ДАННЫЕ, ТОЛЬКО АНАЛИЗИРУЙ ПЕРЕДАННЫЙ СПИСОК!!!!
+            Список коммитов:$text
+            
+            ВАЖНО!!! Если список коммитов пустой, вывести только уведомление, что за последние сутки не было коммитов в репозитории $repo. БОЛЬШЕ НИЧЕГО НЕ ВЫВОДИТЬ!!!!
+        """.trimIndent()
         val message = ChatMessage(
             role = "user",
             content = content,
